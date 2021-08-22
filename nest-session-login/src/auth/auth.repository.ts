d00f720cf95fd5d365841f9e76477431cee8e246
K.Kiyokawa <koichi20110068@gmail.com>
2021-08-22 12:24:13 +0900

    feat: login lock

--- nest-session-login/src/auth/auth.repository.ts
+++ nest-session-login/src/auth/auth.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/db/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthRepository {
@@ -7,5 +8,26 @@ export class AuthRepository {
 
   async findByEmail(email: string) {
     return this.pismaService.user.findUnique({ where: { email } });
+  }
+
+  async update(id: string, data: Partial<User>): Promise<User> {
+    return this.pismaService.user.update({ where: { id }, data });
+  }
+
+  async loginLock(user: User) {
+    return this.update(user.id, {
+      loginLockedAt: new Date(),
+      failedLoginAttempts: 0, // ログインロック解除後に、再び試行回数を与えられるように
+    });
+  }
+
+  async incrementFailedLoginAttempts(user: User) {
+    return this.update(user.id, {
+      failedLoginAttempts: user.failedLoginAttempts + 1,
+    });
+  }
+
+  async resetFailedLoginAttempts(user: User) {
+    return this.update(user.id, { failedLoginAttempts: 0 });
   }
 }
--- nest-session-login/src/auth/auth.service.ts
+++ nest-session-login/src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { compareSync } from 'bcryptjs';
 import { AuthRepository } from './auth.repository';
 
@@ -6,13 +7,54 @@ import { AuthRepository } from './auth.repository';
 export class AuthService {
   constructor(private readonly authRepository: AuthRepository) {}
 
+  MAX_FAILED_ATTEMPTS = 5;
+  LOCK_MINUTES = 10;
+
   async validateUser({ email, password }: { email: string; password: string }) {
     const targetUser = await this.authRepository.findByEmail(email);
 
-    if (targetUser === null || !compareSync(password, targetUser.password))
+    if (targetUser === null) {
+      await new Promise((resolve) => setTimeout(resolve, 300)); // 攻撃者に「このメアドのユーザは存在しない」と感づかれないように
       throw new UnauthorizedException();
+    }
 
-    const { password: _, ...rest } = targetUser;
+    console.log(targetUser);
+
+    // ロックされている or 間違い回数の上限に達しているか
+    if (
+      this.isLoginLocked(targetUser) ||
+      targetUser.failedLoginAttempts >= this.MAX_FAILED_ATTEMPTS
+    ) {
+      console.log(this.isLoginLocked(targetUser));
+      await this.authRepository.loginLock(targetUser);
+      throw new UnauthorizedException({
+        statusCode: 401,
+        code: 'REACH_MAX_FAILED_ATTEMPTS',
+        waitMinutes: this.LOCK_MINUTES,
+      });
+    }
+
+    // パスワードチェック
+    if (!compareSync(password, targetUser.password)) {
+      // パスワードが間違っていたら、試行回数をインクリメント
+      await this.authRepository.incrementFailedLoginAttempts(targetUser);
+      throw new UnauthorizedException();
+    }
+
+    // ログインに成功したら、試行回数をリセット
+    if (targetUser.failedLoginAttempts > 0) {
+      await this.authRepository.resetFailedLoginAttempts(targetUser);
+    }
+
+    const { password: _, ...rest } = targetUser; // パスワードはreturnしない
     return rest;
+  }
+
+  private isLoginLocked(user: User): boolean {
+    if (user.loginLockedAt == null) return false;
+    return (
+      user.loginLockedAt.getTime() + this.LOCK_MINUTES * 60 * 1000 >
+      new Date().getTime()
+    );
   }
 }
