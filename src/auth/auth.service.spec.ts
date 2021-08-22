7104a0aaa9f3d5daba2513d84ccb6797579a8d8f
K.Kiyokawa <koichi20110068@gmail.com>
2021-08-22 11:19:01 +0900

    test: validateUser

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,11 +1,16 @@
+import { UnauthorizedException } from '@nestjs/common';
 import { hashSync } from 'bcryptjs';
 import { PrismaService } from 'src/db/prisma.service';
 import { AuthRepository } from './auth.repository';
 import { AuthService } from './auth.service';
 
 class InMemoryAuthRepository extends AuthRepository {
-  async findByEmail(_email: string) {
-    return { id: '1', email: 'dummy@example.com', password: hashSync('dummy') };
+  users = [
+    { id: '1', email: 'dummy@example.com', password: hashSync('dummy') },
+  ];
+
+  async findByEmail(email: string) {
+    return this.users.find((user) => user.email === email) ?? null;
   }
 }
 
@@ -21,6 +26,24 @@ describe('AuthService', () => {
         password: 'dummy',
       });
       expect(user).toEqual({ id: '1', email: 'dummy@example.com' });
+    });
+
+    it('validate result is not ok (email is wrong)', async () => {
+      await expect(
+        authService.validateUser({
+          email: 'wrong@example.com',
+          password: 'dummy',
+        }),
+      ).rejects.toThrowError(UnauthorizedException);
+    });
+
+    it('validate result is not ok (password is wrong)', async () => {
+      await expect(
+        authService.validateUser({
+          email: 'dummy@example.com',
+          password: 'wrong',
+        }),
+      ).rejects.toThrowError(UnauthorizedException);
     });
   });
 });
