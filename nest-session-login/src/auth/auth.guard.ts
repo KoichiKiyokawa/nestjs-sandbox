43c5a654553c189f32a723b439b240c59ccdb73f
K.Kiyokawa <koichi20110068@gmail.com>
2021-08-26 15:05:47 +0900

    chore: type tweak

--- nest-session-login/src/auth/auth.guard.ts
+++ nest-session-login/src/auth/auth.guard.ts
@@ -3,7 +3,7 @@ import { SessionData } from 'express-session';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+  canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<{ session: SessionData }>();
     return request.session.user != null;
   }
