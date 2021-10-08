77ed01e09555ddec0d752369ee6f781d408ae1c4
K.Kiyokawa <koichi20110068@gmail.com>
2021-10-08 23:18:38 +0900

    chore(nest-typeorm-graphql): ormconfig

--- nest-typeorm-graphql/src/app.module.ts
+++ nest-typeorm-graphql/src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import ormConfigBase from '../ormconfig';
+import ormConfig from '../ormconfig';
 import { AuthModule } from './auth/auth.module';
 import { CommentModule } from './comment/comment.module';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
-
-// migrations の設定があると、start:devをしたときにtsファイルを読み込みに行ってエラーがでる
-const { migrations: _, ...ormConfig } = ormConfigBase;
 
 @Module({
   imports: [
@@ -16,7 +13,8 @@ const { migrations: _, ...ormConfig } = ormConfigBase;
       autoSchemaFile:
         process.env.NODE_ENV === 'production' ? true : 'schema.gql',
     }),
-    TypeOrmModule.forRoot(ormConfig),
+    // migrations の設定があると、start:devをしたときにtsファイルを読み込みに行ってエラーがでる
+    TypeOrmModule.forRoot({ ...ormConfig, migrations: undefined }),
     UserModule,
     PostModule,
     CommentModule,
